a5a6262052b77c2a9a10063c281fdfae4df9ccb2
Sudhakar <84635136+sudhakar-71@users.noreply.github.com>
2023-10-04 16:01:42 +0530

    Create llama.py

---
+++ llama.py
@@ -0,0 +1,20 @@
+import torch
+import safetensors
+from transformers import LlamaTokenizer, LlamaForCausalLM
+
+model_path = 'openlm-research/open_llama_3b'
+# model_path = 'openlm-research/open_llama_7b'
+#model_path = 'openlm-research/open_llama_13b'
+
+tokenizer = LlamaTokenizer.from_pretrained(model_path)
+model = LlamaForCausalLM.from_pretrained(
+    model_path, torch_dtype=torch.float16, offload_folder="offload",
+)
+
+prompt = 'Q: What is the largest animal?\nA:'
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+generation_output = model.generate(
+    input_ids=input_ids, max_new_tokens=32
+)
+print(tokenizer.decode(generation_output[0]))
